a3b6685afc59f787b77ccdac7bd278353d2bc9a9
Hovhannes <hhovhannisyan348@gmail.com>
2026-05-04 15:12:20 +0400

    all project update

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import { initFAQ } from './modules/faq.js';
 import { initForm } from './modules/form.js';
 import { initMenu } from './modules/menu.js';
 import { initSlider } from './modules/slider.js';
+import { sliderManager } from './modules/slider.js';
 
 async function loadComponent(id, file) {
   const el = document.getElementById(id);
@@ -19,6 +20,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   // 1. Загружаем компоненты (синхронно по порядку)
   await loadComponent('header', 'components/header.html');
   await loadComponent('footer', 'components/footer.html');
+  await loadComponent('content', 'content.html')
 
   // 2. Инициализация UI (после полной вставки DOM)
   initDropdown(document);
@@ -26,4 +28,13 @@ document.addEventListener('DOMContentLoaded', async () => {
   initFAQ();
   initForm();
   initMenu();
+  sliderManager.register({
+    selector: '.target__box',
+    pagination: '.target__box .swiper-pagination'
+  });
+
+  sliderManager.register({
+    selector: '.services__box',
+    pagination: '.services__box .swiper-pagination'
+  });
 });
--- js/modules/slider.js
+++ js/modules/slider.js
@@ -17,3 +17,59 @@ export function initSlider() {
     }
   });
 }
+
+
+class SliderManager {
+  constructor() {
+    this.instances = new Map();
+    this.configs = [];
+
+    this.media = window.matchMedia('(max-width: 480px)');
+    this.media.addEventListener('change', this.handleChange.bind(this));
+  }
+
+  register({ selector, pagination, options = {} }) {
+    const el = document.querySelector(selector);
+    if (!el) return;
+
+    this.configs.push({ selector, pagination, options, el });
+
+    // первая инициализация
+    this.toggle({ selector, pagination, options, el });
+  }
+
+  handleChange() {
+    this.configs.forEach(config => this.toggle(config));
+  }
+
+  toggle(config) {
+    const { selector, pagination, options, el } = config;
+    const isMobile = this.media.matches;
+    const instance = this.instances.get(selector);
+
+    if (isMobile && !instance) {
+      const slider = new Swiper(el, {
+        slidesPerView: "auto",
+        spaceBetween: 1.8,
+        grabCursor: true,
+        loop: false,
+
+        pagination: {
+          el: pagination,
+          clickable: true,
+        },
+
+        ...options
+      });
+
+      this.instances.set(selector, slider);
+    }
+
+    if (!isMobile && instance) {
+      instance.destroy(true, true);
+      this.instances.delete(selector);
+    }
+  }
+}
+
+export const sliderManager = new SliderManager();
